b3926ecec01cd0e209e91b199a25ff487c50c0ad
blidormf <150001374+blidormf@users.noreply.github.com>
2025-02-19 19:06:10 +0530

    Refactor to use classes

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,119 @@
-const library = [];
+class Book {
+    title;
+    author;
+    pages;
+    isRead;
+    domElement;
 
-function Book(title, author, pages, isRead = false, domElement) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
-    this.domElement = domElement;
+    constructor(title, author, pages, isRead = false) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead;
+        this.domElement = null;
+    }
+}
+
+class Library {
+    bookList;
+
+    constructor() {
+        this.bookList = [];
+    }
+
+    addBook(book) {
+        // Main container
+        const booksContainer = document.querySelector('.books-container');
+    
+        // Book card
+        const bookCard = document.createElement('div');
+        bookCard.className = 'book-card';
+        bookCard.style.backgroundColor = getRandomLightColor();
+    
+        // Children of the book card
+        const bookCardLeft = document.createElement('div');
+        bookCardLeft.className = 'book-card-left';
+    
+        const bookCardRight = document.createElement('div');
+        bookCardRight.className = 'book-card-right';
+    
+        // Children of the left part of the book card
+        const bookIcon = document.createElement('object');
+        bookIcon.type = 'image/svg+xml';
+        bookIcon.data = 'assets/book-icon.svg';
+    
+        // Children of the right part of the book card
+        const bookInfo = document.createElement('div');
+        bookInfo.className = 'book-info';
+    
+        const bookActions = document.createElement('div');
+        bookActions.className = 'book-actions';
+    
+        // Children of book info
+        const title = document.createElement('p');
+        title.className = 'title';
+        title.textContent = book.title;
+    
+        const author = document.createElement('p');
+        author.className = 'author';
+        author.textContent = book.author;
+    
+        const pages = document.createElement('p');
+        pages.className = 'pages';
+        pages.textContent = book.pages + ' ' + (book.pages > 1 ? 'pages' : 'page');
+    
+        const readStatus = document.createElement('p');
+        readStatus.className = 'read';
+        readStatus.textContent = book.isRead ? 'Read' : 'Not read yet';
+    
+        // Append the book info elements to the book info section
+        bookInfo.appendChild(title);
+        bookInfo.appendChild(author);
+        bookInfo.appendChild(pages);
+        bookInfo.appendChild(readStatus);
+    
+        // Children of book actions
+        const readButton = document.createElement('button');
+        readButton.className = 'read';
+        readButton.textContent = 'Read';
+    
+        const deleteButton = document.createElement('button');
+        deleteButton.className = 'delete';
+        deleteButton.textContent = 'Delete';
+    
+        // Create element hierarchy
+        booksContainer.appendChild(bookCard);
+        bookCard.appendChild(bookCardLeft);
+        bookCardLeft.appendChild(bookIcon);
+        bookCard.appendChild(bookCardRight);
+        bookCardRight.appendChild(bookInfo);
+        bookCardRight.appendChild(bookActions);
+        bookActions.appendChild(readButton);
+        bookActions.appendChild(deleteButton);
+    
+        // Add DOM element
+        book.domElement = bookCard;
+    
+        deleteButton.addEventListener('click', () => {
+            bookCard.classList.add('fade-out');
+            this.bookList.splice(this.bookList.indexOf(book), 1);
+            bookCard.addEventListener('transitionend', function () {
+                bookCard.remove();
+            });
+        });
+    
+        readButton.addEventListener('click', () => {
+            if (!book.isRead) {
+                book.isRead = true;
+                readStatus.textContent = 'Read';
+                readButton.textContent = 'Unread';
+            } else {
+                book.isRead = false;
+                readStatus.textContent = 'Not read yet';
+                readButton.textContent = 'Read';
+            }
+        });
+    }
 }
 
 function getRandomLightColor() {
@@ -18,101 +126,7 @@ function getRandomLightColor() {
     return `rgb(${r}, ${g}, ${b})`;
 }
 
-function addBook(book) {
-    // Main container
-    const booksContainer = document.querySelector('.books-container');
-
-    // Book card
-    const bookCard = document.createElement('div');
-    bookCard.className = 'book-card';
-    bookCard.style.backgroundColor = getRandomLightColor();
-
-    // Children of the book card
-    const bookCardLeft = document.createElement('div');
-    bookCardLeft.className = 'book-card-left';
-
-    const bookCardRight = document.createElement('div');
-    bookCardRight.className = 'book-card-right';
-
-    // Children of the left part of the book card
-    const bookIcon = document.createElement('object');
-    bookIcon.type = 'image/svg+xml';
-    bookIcon.data = 'assets/book-icon.svg';
-
-    // Children of the right part of the book card
-    const bookInfo = document.createElement('div');
-    bookInfo.className = 'book-info';
-
-    const bookActions = document.createElement('book-actions');
-    bookActions.className = 'book-actions';
-
-    // Children of book info
-    const title = document.createElement('p');
-    title.className = 'title';
-    title.textContent = book.title;
-
-    const author = document.createElement('p');
-    author.className = 'author';
-    author.textContent = book.author;
-
-    const pages = document.createElement('p');
-    pages.className = 'pages';
-    pages.textContent = book.pages + ' ' + (book.pages > 1 ? 'pages' : 'page');
-
-    const readStatus = document.createElement('p');
-    readStatus.className = 'read';
-    readStatus.textContent = book.isRead ? 'Read' : 'Not read yet';
-
-    // Append the book info elements to the book info section
-    bookInfo.appendChild(title);
-    bookInfo.appendChild(author);
-    bookInfo.appendChild(pages);
-    bookInfo.appendChild(readStatus);
-
-    // Children of book actions
-    const readButton = document.createElement('button');
-    readButton.className = 'read';
-    readButton.textContent = 'Read';
-
-    const deleteButton = document.createElement('button');
-    deleteButton.className = 'delete';
-    deleteButton.textContent = 'Delete';
-
-    // Create element hierarchy
-    booksContainer.appendChild(bookCard);
-    bookCard.appendChild(bookCardLeft);
-    bookCardLeft.appendChild(bookIcon);
-    bookCard.appendChild(bookCardRight);
-    bookCardRight.appendChild(bookInfo);
-    bookCardRight.appendChild(bookActions);
-    bookActions.appendChild(readButton);
-    bookActions.appendChild(deleteButton);
-
-    // Add DOM element
-    book.domElement = bookCard;
-
-    deleteButton.addEventListener('click', () => {
-        bookCard.classList.add('fade-out');
-        library.splice(library.indexOf(book), 1);
-        bookCard.addEventListener('transitionend', function () {
-            bookCard.remove();
-        });
-
-        // booksContainer.removeChild(bookCard);
-    });
-
-    readButton.addEventListener('click', () => {
-        if (!book.isRead) {
-            book.isRead = true;
-            readStatus.textContent = 'Read';
-            readButton.textContent = 'Unread';
-        } else {
-            book.isRead = false;
-            readStatus.textContent = 'Not read yet';
-            readButton.textContent = 'Read';
-        }
-    });
-}
+const library = new Library();
 
 const addButton = document.querySelector('.actions .add-button');
 const addBookDialog = document.querySelector('dialog.add-book-dialog');
@@ -131,7 +145,7 @@ addButton.addEventListener('click', () => {
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    addBook(new Book(title.value, author.value, pages.value, isRead.checked));
+    library.addBook(new Book(title.value, author.value, pages.value, isRead.checked));
 
     title.value = author.value = pages.value = "";
     isRead.checked = false;
